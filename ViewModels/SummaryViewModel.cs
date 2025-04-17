56fb51a2d234b06733f13a45cfd501dca204e48e
Jimmy White <jimmyeao@users.noreply.github.com>
2025-04-17 21:01:13 +0100

    Fix text wrapping

--- ViewModels/SummaryViewModel.cs
+++ ViewModels/SummaryViewModel.cs
@@ -183,6 +183,7 @@ namespace EliteInfoPanel.ViewModels
                         PackIconKind.SpaceStation)
                     {
                         FontSize = fontSize
+                                                     
                     });
                 }
 
